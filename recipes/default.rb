f5876ef5bb6161222567ef3519fba37f53d14d28
Sebastian Johnsson <sebastian@agiley.se>
2012-09-05 11:51:54 +0200

    Updating host recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,15 +1,21 @@
+hostname = node[:hosts][:hostname].gsub(/[ _\.]/, "-")
+
 template "/etc/hostname" do
   source "hostname.erb"
   owner "root"
   group "root"
   mode 0644
+  variables :hostname => hostname
 end
-
-execute "hostname -F /etc/hostname"
 
 template "/etc/hosts" do
   source "hosts.erb"
   owner "root"
   group "root"
   mode 0644
+  variables :hostname => hostname
+end
+
+execute "setting_hostname" do
+  command "hostname #{hostname}"
 end
